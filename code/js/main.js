f933a388f20b30fb8e2bbc34d34a644e583d075f
Pablo <pabloleonpsico@gmail.com>
2018-08-02 14:36:55 +0200

    Vue first steps

--- code/js/main.js
+++ code/js/main.js
@@ -1,19 +1,64 @@
-window.onload = function(){
+window.onload = function() {
+    Vue.filter('uppercase', (value)=>value.toUpperCase());
     new Vue({
-        el: 'main',
-        data:{
-            text: 'Hello from Vue2',
-            name: 'default name',
-            note: 5,
-            superheroes: ['Iroman', 'Dr. Strange','Hulk','Wolverine','Magneto'],
-            fruits:[
-                {name: 'Manzana', season:'Invierno', price: 'Bajo'},
-                {name: 'Naranja', season:'Otoño', price: 'Medio'},
-                {name: 'Cereza', season:'Primavera', price: 'Alto'},
-                {name: 'Melón', season:'Verano', price: 'Bajo'},
-                {name: 'Sandía', season:'Verano', price: 'Medio'}
-            ]
-        }
+    el: "main",
+    /**Mounted is like $(document).ready */
+    mounted(){
+        axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+            this.posts = response.data;
         });
-}
-/** v-model allow two way data-binding */
+    },
+    data: {
+      posts: null,
+      text: "Hello from Vue2",
+      name: "Pablo",
+      surname: "Leon",
+      note: 5,
+      superheroes: ["Iroman", "Dr. Strange", "Hulk", "Wolverine", "Magneto"],
+      newHero: null,
+      search: '',
+      confirm: null,
+      fruits: [
+        { name: "Manzana", season: "Invierno", price: "Bajo" },
+        { name: "Naranja", season: "Otoño", price: "Medio" },
+        { name: "Cereza", season: "Primavera", price: "Alto" },
+        { name: "Melón", season: "Verano", price: "Bajo" },
+        { name: "Sandía", season: "Verano", price: "Medio" }
+      ],
+      special_fruit: { name: "Aguacate", season: "Verano", price: "Muy Alto" }
+
+    },
+    methods: {
+      add_hero() {
+        this.superheroes.unshift(this.newHero);
+        this.newHero = null;
+      },
+      remove_hero(index) {
+        this.superheroes.splice(index, 1);
+      },
+      mark(index){
+          this.confirm = index;
+      }
+    },
+    computed:{
+        nameAndSurname(){
+            let date = new Date();
+            let year = date.getFullYear();
+            return this.name + " " + this.surname + " - Nota: " + this.note + " - " + "Año: "+ year;
+        },
+        orderedHeroes(){
+            let orderedList = this.superheroes.concat().sort();
+            return orderedList;
+        },
+        searchFruit(){
+            return this.fruits.filter((fruit)=>  fruit.name.includes(this.search));
+        }
+    }
+  });
+  const vue2 = new Vue({
+    el: '#app',
+    data:{
+        text: 'Segunda instancia de Vue'
+    }
+  });
+};
